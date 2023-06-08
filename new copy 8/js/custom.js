d6b1f0b48877e2a83b6a2667394c0627cd4c0e99
yasmen-khaled <yasmen.khaled2000@gmail.com>
2023-06-08 18:27:11 +0200

    Update Html file

--- new copy 8/js/custom.js
+++ new copy 8/js/custom.js
@@ -61,3 +61,48 @@ $(function () {
 
 });
 
+
+
+
+
+function searchDonors() {
+	// Get input value and convert to lowercase for case-insensitive search
+	var inputValue = document.getElementById("searchInput").value.toLowerCase();
+	
+	// Get list of card elements
+	var cardList = document.getElementsByClassName("card");
+	
+	// Loop through cards and hide/show based on search input
+	for (var i = 0; i < cardList.length; i++) {
+	  var donorName = cardList[i].getElementsByTagName("h2")[0].innerHTML.toLowerCase();
+	  if (donorName.indexOf(inputValue) > -1) {
+		cardList[i].style.display = "";
+	  } else {
+		cardList[i].style.display = "none";
+	  }
+	}
+  }
+  
+  
+  
+  function changeStyle(styleName) {
+	 var styleSheets = document.getElementsByTagName('link');
+	 var styleFound = false;
+	 for (var i = 0; i < styleSheets.length; i++) {
+		if (styleSheets[i].href.includes(styleName)) {
+		   styleSheets[i].disabled = false;
+		   styleFound = true;
+		}      else {
+		   styleSheets[i].disabled = true;
+		}
+	 }
+	 if (!styleFound) {
+		var link = document.createElement('link');
+		link.href = styleName;
+		link.type = 'text/css';
+		link.rel = 'style2';
+		document.head.appendChild(link);
+	 }
+  }
+  
+  
